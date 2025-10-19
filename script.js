7ed2568eb1f9f31bd9a4f7f7e24d84da665db0bb
Nishant <111058691+NiSHAant26@users.noreply.github.com>
2025-10-19 20:09:11 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// Load header.html into each page
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      const headerContainer = document.createElement('header');
+      headerContainer.innerHTML = data;
+      document.body.prepend(headerContainer);
+    })
+    .catch(err => console.error('Error loading header:', err));
+});
